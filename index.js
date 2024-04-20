f87281917c1e447c01bf5eb5a4b1c230b2ff2a7f
no1xa <134647793+no1xa@users.noreply.github.com>
2024-04-20 23:38:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,18 +6,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    // check if message isn't from us
-    if (msg.author == client.user) {
-      return;
-    }
-    else if (msg.content === 'ping') {
-      msg.reply('Pong!');
-    }
-    else {
-        msg.reply(msg.content);
-      console.log(msg.content)
-    }
-});
+
+
 
 client.login(process.env.TOKEN); //login bot using token
